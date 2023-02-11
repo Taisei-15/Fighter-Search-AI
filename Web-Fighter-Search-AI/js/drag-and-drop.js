e3adc38f938d719aeb3bff905816942d4b1290a7
taisei <hama400@gmail.com>
2023-02-11 09:23:24 +0900

    戦闘機分類AIを作成した

---
+++ Web-Fighter-Search-AI/js/drag-and-drop.js
@@ -0,0 +1,43 @@
+const dropzone = document.getElementById("dropzone");
+const fileInput = document.getElementById("file-input");
+const submitButton = document.getElementById("submit-button");
+const preview = document.getElementById("preview");
+
+dropzone.addEventListener("dragover", function(event) {
+  event.preventDefault();
+});
+
+dropzone.addEventListener("drop", function(event) {
+  event.preventDefault();
+  const files = event.dataTransfer.files;
+  handleFiles(files);
+});
+
+fileInput.addEventListener("change", function() {
+  const files = this.files;
+  handleFiles(files);
+});
+
+dropzone.addEventListener("click", function() {
+  fileInput.click();
+});
+
+submitButton.addEventListener("click", function() {
+  const image = preview.querySelector("img");
+  if (image) {
+    // Submit the image here
+    alert("Image submitted!");
+  }
+});
+
+function handleFiles(files) {
+  const file = files[0];
+  const reader = new FileReader();
+  reader.addEventListener("load", function() {
+    const image = new Image();
+    image.src = this.result;
+    preview.appendChild(image);
+    submitButton.removeAttribute("disabled");
+  });
+  reader.readAsDataURL(file);
+}
