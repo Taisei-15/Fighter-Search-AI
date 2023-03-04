7b9aa0767cfe5a1ae8a070501d28267f5b12bfcb
taisei <hama400@gmail.com>
2023-03-04 16:44:27 +0900

    文字の点滅を検証する

--- Web-Fighter-Search-AI/js/drag-and-drop.js
+++ Web-Fighter-Search-AI/js/drag-and-drop.js
@@ -1,5 +1,5 @@
 const dropzone = document.getElementById("dropzone");
-const fileInput = document.getElementById("file-input");
+
 const submitButton = document.getElementById("submit-button");
 const preview = document.getElementById("preview");
 
@@ -36,31 +36,12 @@ dropzone.addEventListener("drop", function(event) {
     }
 });
 
-fileInput.addEventListener("change", function() {
-  const files = this.files;
-  handleFiles(files);
-});
 
-dropzone.addEventListener("click", function() {
-  fileInput.click();
-});
 
 submitButton.addEventListener("click", function() {
-  const image = preview.querySelector("img");
-  if (image) {
+  
     // Submit the image here
-    alert("解析中‼");
-  }
+  alert("解析中‼");
+  
 });
 
-function handleFiles(files) {
-  const file = files[0];
-  const reader = new FileReader();
-  reader.addEventListener("load", function() {
-    const image = new Image();
-    image.src = this.result;
-    preview.appendChild(image);
-    submitButton.removeAttribute("disabled");
-  });
-  reader.readAsDataURL(file);
-}
